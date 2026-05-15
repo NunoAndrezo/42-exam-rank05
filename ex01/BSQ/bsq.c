f7885f873d8bda330a33100e0da5f8f61ff5f83a
nuno <nunoandrezo97@gmail.com>
2026-05-15 22:17:40 +0100

    bsq: in the making

--- ex01/BSQ/bsq.c
+++ ex01/BSQ/bsq.c
@@ -1,35 +1,55 @@
 #include "bsq.h"
 
-static int execute_bsq(FILE *file);
-static int check_file(char *f);
+static int execute_bsq(FILE *file, t_bsq *bsq);
+static void init_bsq(t_bsq *bsq);
+
+/* 
+FILE *file = fopen(path, "r");
+
+int matrix[height][width];
+
+min = (matrix[y - 1][x], matrix[y][x - 1], matrix[y - 1][x - 1]) + 1
+find_min()
+
+find_biggest();
+ */
 
 int	main(int ac, char *av[])
 {
+	t_bsq bsq;
+
+	init_bsq(&bsq);
 	if (ac == 1)
 	{
-		if (execute_bsq(stdin))
+		if (execute_bsq(stdin, &bsq))
 			return (fprintf(stderr, "map error\n"), 1);
 	}
 	else if (ac == 2)
 	{
-		if (check_file(av[1]))
+		FILE *file;
+		if ((file = fopen(av[1], "r")) == NULL)
 			return (fprintf(stderr, "map error\n"), 1);
+		if (execute_bsq(file, &bsq))
+			return (fprintf(stderr, "map error\n"), 1);
+		fclose(file);
 	}
 	else
 		return (fprintf(stderr, "map error\n"), 1);
 	return (0);
 }
 
-static int execute_bsq(FILE *file)
+static void init_bsq(t_bsq *bsq)
 {
-	char *line = NULL;
-	size_t n;
-	if (getline(&line, &n, file) == -1)
-		return -1;
-	
+	bsq->n_lines = 0;
+
+	bsq->map = NULL;
+
+	bsq->x_start = 0;
+	bsq->y_start = 0;
+	bsq->biggest = 0;
 }
 
-static int check_file(char *s)
+static int execute_bsq(FILE *file, t_bsq *bsq)
 {
 
 }
--- ex01/BSQ/bsq.h
+++ ex01/BSQ/bsq.h
@@ -4,7 +4,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
 
+/*  
+ - The number of lines on the map;
+ - The "empty" character;
+ - The "obstacle" character;
+ - The "full" character. 
+*/
+typedef struct s_bsq
+{
+	int		n_lines;
+	char	empty;
+	char	obstacle;
+	char	full;
 
+	char	**map;
+
+	int		x_start;
+	int		y_start;
+	int		biggest;
+} t_bsq;
 
 #endif
